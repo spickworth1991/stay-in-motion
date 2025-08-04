b1ffb17d88b58eddd8f1a6455862a60fa14a8c4a
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2025-08-04 14:57:48 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
+  darkMode: 'class', 
   theme: {
     extend: {
       colors: {
