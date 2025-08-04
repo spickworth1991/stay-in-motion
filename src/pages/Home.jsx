5f9c4cf0b64b0472fd35797fa52fb369fbb00e9c
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2025-08-04 15:30:08 -0400

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -71,7 +71,7 @@ export default function Home() {
                 inline-block
                 px-8 py-4
                 bg-gradient-to-r from-primary to-accent
-                text-white font-semibold rounded-full
+                text-white dark:text-gray-200 font-semibold rounded-full
                 shadow-lg transform transition-transform duration-300
                 hover:from-accent hover:to-primary hover:-translate-y-1
               "
