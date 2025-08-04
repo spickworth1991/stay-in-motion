b5e336a0646ace94975d573434f8ff4d673c2037
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2025-08-04 13:45:12 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        cream:   '#f6dfcdff',
+        cream:   '#f8e8dcff',
         primary: '#3cadbaff',
         accent:  '#f3d0a0ff',
       },
