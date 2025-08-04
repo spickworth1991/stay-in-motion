dd9eb0de89f7592122bfd5099d8ab9d23c357c20
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2025-08-04 12:35:48 -0400

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="fixed inset-x-0 top-0 z-50 h-16 bg-cream/90 backdrop-blur shadow-md">
+      <nav class="fixed w-full z-50 bg-white/30 backdrop-blur-md shadow-md">
         <div className="max-w-7xl mx-auto flex h-full items-center justify-between px-4 md:px-8">
           <Link to="/" className="flex items-center">
             <img src="/logo_navbar.png" alt="" className="h-8 w-8" />
